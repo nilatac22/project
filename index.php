e98bdc5a078a2348196427ac568e438efd532c15
nilatac22 <nilatac22@gmail.com>
2021-06-23 12:14:50 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title>Google Cloud Project</title>
+<style>
+	body {
+	margin: 0;
+	}
+	.header {
+	padding: 60px;
+	text-align: center;
+	background: #1abc9c;
+	color: white;
+	font-size: 25px;
+	}
+	.content {padding:20px;}
+	h1 {
+		text-align: center;
+	}
+</style>
+</head>
+<body>
+<div class="header">
+  <h1>Google Cloud Platform Project</h1>
+</div>
+<div class="content">
+<h1>This website is being served from <?php echo gethostname(); ?> </h1>
+</div>
+</body>
+</html>
